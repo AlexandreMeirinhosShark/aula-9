585cc386ad291c8688eeed798b080d644363999b
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2024-11-22 19:30:06 +0000

    Update primonaoprimo.py

--- primonaoprimo.py
+++ primonaoprimo.py
@@ -1,18 +1,21 @@
 from time import sleep
 
 
-def comp_print(text):
+def comp_print(text, skips):
     for a in str(text):
         print(a, end="")
         sleep(0.1)
+    if skips > 0:
+        for b in range(int(skips)):
+            print()
 
 
 numofdivs = 0
 
-comp_print("Digite o seu número: ")
+comp_print("Digite o seu número: ", 0)
 num = int(input())
 
-comp_print(f"Os números divisores de {num} são: ")
+comp_print(f"Os números divisores de {num} são: ", 0)
 
 for i in range(num):
     if num % (i+1) == 0:
@@ -21,9 +24,8 @@ for i in range(num):
         numofdivs += 1
 
 print()
-comp_print(f"{num} tem {numofdivs} divisores.")
-print()
+comp_print(f"{num} tem {numofdivs} divisores.", 1)
 if numofdivs == 2:
-    comp_print("Isto quer dizer que esse número é primo. Parabéns!")
+    comp_print("Isto quer dizer que esse número é primo. Parabéns!", 0)
 else:
-    comp_print("O seu número não é primo.")
+    comp_print("O seu número não é primo.", 0)
