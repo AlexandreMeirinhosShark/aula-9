073fd0bef73923f1eec6a5cd04f4419c22f6ad8f
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2024-11-22 19:34:27 +0000

    Add files via upload

---
+++ desafioextra.py
@@ -0,0 +1,7 @@
+"""
+DESAFIO: O Jogo dos Números Mágicos
+
+O objetivo é adivinhar o número mágico, mas há mais!
+O programa vat dar-te dicas, dizer se estás perto ou longe, e até contar as tuas tentativas.
+Vais conseguir resolver?
+"""
